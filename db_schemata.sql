6f8326287d23a819cd95349f72abce48c7b0ac2e
Johann <jvillalv@iu.edu>
2021-07-23 14:21:14 -0400

    fixed database uri to connect to heroku's

---
+++ db_schemata.sql
@@ -0,0 +1,9 @@
+create table cleaned_2018_stats(
+	id serial primary key,
+	name varchar,
+	price float,
+	market_cap_millions float,
+	reddit_subscribers float,
+	twitter_followers float,
+	alexa_rank float	
+)
