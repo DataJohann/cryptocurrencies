8211c61c1d51b918e31b58ef9bc2793b083dddb2
Johann <jvillalv@iu.edu>
2021-08-11 14:44:21 -0400

    bar charts y axis is formated to have highest y value from years in question as the mas value in scale in order to compare side by side (thanks Eric) Alexa rank is givin issues

--- crypto/static/js/plot.js
+++ crypto/static/js/plot.js
@@ -3,7 +3,7 @@
 
 // Fetch the JSON data for 2018
 
-function build_2018_charts(token){
+function build_2018_charts(token, reddit_max, twitter_max, alexa_max){
     const url_2018 = "/api/data/2018";
     d3.json(url_2018).then(function(data) {
 
@@ -37,9 +37,21 @@ function build_2018_charts(token){
         
         }
 
-        build_plot(crypto_names, reddit_y, 'reddit', year)
-        build_plot(crypto_names, twitter_y, 'twitter', year)
-        build_plot(crypto_names, alexa_y, 'alexa', year)
+        // Calculate max value for y axis
+        reddit_max > Math.max.apply(Math, reddit_y) ? reddit_max = reddit_max : reddit_max = Math.max.apply(Math, reddit_y)
+        twitter_max > Math.max.apply(Math, twitter_y) ? twitter_max = twitter_max : twitter_max = Math.max.apply(Math, twitter_y)
+        alexa_max > Math.max.apply(Math, alexa_y) ? alexa_max = alexa_max : () => { alexa_max = Math.max.apply(Math, alexa_y)
+            return console.log("test")
+        }
+
+        // console.log(Math.max(parseInt(alexa_y)))
+        // twitter_max = Math.max(parseInt(twitter_y))
+        // alexa_max = Math.max(parseInt(alexa_y))
+
+
+        build_plot(crypto_names, reddit_y, 'reddit', year, reddit_max)
+        build_plot(crypto_names, twitter_y, 'twitter', year, twitter_max)
+        build_plot(crypto_names, alexa_y, 'alexa', year, alexa_max)
     });
 };
 
@@ -80,12 +92,23 @@ function build_2021_charts(token){
     
         }
 
+        // Obtain the y maximulum values
+        // Send the maximum value to the other chart, compare and pass the biggest one to the build plot chart to normalize data
+        reddit_max = Math.max.apply(Math, reddit_y)
+        twitter_max = Math.max.apply(Math, twitter_y)
+        alexa_max = Math.max.apply(Math, alexa_y)
         
+        // console.log(Math.max.apply(Math, alexa_y))
+
+        build_2018_charts(token, reddit_max, twitter_max, alexa_max)
+
         build_plot(crypto_names, reddit_y, 'reddit', year)
         build_plot(crypto_names, twitter_y, 'twitter', year)
         build_plot(crypto_names, alexa_y, 'alexa', year)
     });
 };
+
+// Function to define the color of the chart
 function define_color(social_media){
     if (social_media == 'reddit'){
         return 'rgb(255,69,0)'
@@ -96,11 +119,54 @@ function define_color(social_media){
     }
 }
 
-// define_color('reddit');
+// function to find the y max value to normalize the chart y axis view
 
+function y_scale(token, year_a, year_b){
 
+    years = [year_a, year_b]
+    var max_values = []
+    max_value = []
+    for (var x = 0; x < years.length; x++){
 
-function build_plot(x_values, y_values, social, year){
+        const url = `/api/data/${years[x]}`;
+        d3.json(url).then(function (data) {
+            //  Create an array with the data for the selected token
+            var resultArray = data.filter(tokenObj => tokenObj.name == token);
+
+            //  Check to see if the result array has no results, if so, it means user wants to see all tokens in graph
+            // 
+            resultArray == 0 ? data = data : data = resultArray;
+            
+            max_y_value = 0
+        
+            for (var i = 0; i < data.length; i++){
+                    var alexa_int = data[i].alexa_rank
+                    alexa_int > max_y_value ? max_y_value = alexa_int : max_y_value = max_y_value
+                    // console.log(alexa_int)
+            }
+
+            max_values.push(max_y_value)
+            
+            // console.log(Math.max(max_y_value))
+
+        } ) 
+    } console.log(Math.max(max_values))
+    max_values.forEach(function(item, index, array){
+        console.log(item, index)
+    })
+    var max_y = 0
+    setTimeout(function (){
+        max_y = Math.max.apply(Math, max_values)
+        max_value.push(max_y)
+        console.log(Math.max.apply(Math, max_values)) }, 10000)
+
+    console.log(max_value)
+    
+}
+
+// Build bar plots
+
+function build_plot(x_values, y_values, social, year, y_max){
 
 
     var trace1 = {
@@ -115,8 +181,11 @@ function build_plot(x_values, y_values, social, year){
     data = [
         trace1
     ];
+    var layout = {
+        yaxis: {range: [0, y_max]}
+      };
 
-    Plotly.newPlot(`${social}-plot${year}`, data);
+    Plotly.newPlot(`${social}-plot${year}`, data, layout);
 
 };
 
@@ -126,32 +195,22 @@ function init() {
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
   
-    // Use the list of sample names to populate the select options
+    // Use the list of sample names to populate the dropdown menu options
     d3.json("/api/data/2018").then((data) => {
-      var sampleNames = data.filter(dataObj => dataObj.name);
-        
     
-    //   console.log(names)
-      
-    //   sampleNames.forEach((sample) => {
-    //     console.log(sample)
-    //     selector
-    //       .append("option")
-    //       .text(sample.name)
-    //       .property("value", sample);
-    //   });
+    // Add 'All option to see all entries
     selector
     .append("option")
     .text("All")
-      Object.entries(data).forEach(([key, value]) =>{
-          selector
-            .append("option")
-            .text(value.name)
-            .property("value", value.name)
-      })
-     
+    Object.entries(data).forEach(([key, value]) =>{
+        selector
+        .append("option")
+        .text(value.name)
+        .property("value", value.name)
+    })
     
-    build_2018_charts();
+    
+    // build_2018_charts();
     build_2021_charts();
 
     });
@@ -166,3 +225,5 @@ function optionChanged(newToken){
 }
 
 init()
+
+y_scale('', '2018', '2021');
